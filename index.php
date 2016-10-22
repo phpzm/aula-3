bd4c3b7240c52906f0fc65c815317042e37f44bc
William <wilcorrea@gmail.com>
2016-10-22 13:48:41 -0200

    Remover CDN

--- index.php
+++ index.php
@@ -9,7 +9,6 @@ require_once 'vendor/autoload.php';
     <head>
         <meta charset="utf-8">
         <title>Meu Curso Legal</title>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css" integrity="sha256-F7gqKszCwmz8vhiti+AICU8dLfIEpxzPVihhhGfbbKg=" crossorigin="anonymous" />
     </head>
     <body>
         <?php
