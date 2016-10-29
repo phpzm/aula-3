18bf8de9b8cf3d8a903ef916f2da6f415b22cd0e
William <wilcorrea@gmail.com>
2016-10-29 15:30:33 -0200

    Commit & push

--- src/controller/UsuarioController.php
+++ src/controller/UsuarioController.php
@@ -9,5 +9,17 @@ class UsuarioController
     public function salvar(Input $input)
     {
         var_dump($input);
+        ?>
+            <div class="section">
+                <div>
+                    Registo salvo com sucesso!
+                </div>
+                <div>
+                    <a class="button" href="<?php route('exercicio/6'); ?>">
+                        Voltar
+                    </a>
+                </div>
+            </div>
+        <?php
     }
 }
